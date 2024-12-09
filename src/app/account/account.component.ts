af9dec1e2217f1af9c7626e94a3a6af5fa37c8a7
Bindhusha Sadasivan <bindhushasadasivan@gmail.com>
2024-12-09 06:08:22 +0530

    107 - Injecting the Logging Service into Components

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,19 +1,29 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LoggingServiceService } from '../shared-service/logging-service/logging-service.service';
 
 @Component({
   selector: 'app-account',
   standalone: true,
   imports: [],
+  providers:[LoggingServiceService],
   templateUrl: './account.component.html',
   styleUrl: './account.component.css'
 })
 export class AccountComponent {
 @Input() account!:{name:string, status:string};
 @Input() id!:number;
-@Output() statusChanged=new EventEmitter<{id:number,status:string}>()
+@Output() statusChanged=new EventEmitter<{id:number,status:string}>();
+
+/**
+ *
+ */
+constructor(private loggingService:LoggingServiceService) {
+}
 
 setTo(status:string){
   this.statusChanged.emit({id:this.id, status:status});
-  this.statusChanged.subscribe(data => console.log(data));
+  // this.statusChanged.subscribe(data => console.log(data));
+
+  this.loggingService.logMessage(status);
 }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -12,13 +12,23 @@ import { LoggingServiceService } from '../shared-service/logging-service/logging
 export class NewAccountComponent {
   @Output() accoutAdded = new EventEmitter<{name:string, status:string}>();
 
+  /**
+   * Call the service in the constructor
+   */
+  constructor(private loggingService:LoggingServiceService) {
+
+  }
+
   onAddAccount(accountName:string, accountStatus:string){
     this.accoutAdded.emit({name:accountName, status:accountStatus});
     // this.accoutAdded.subscribe(data => console.log(data));
     // console.log('A server status changed, new status:' + accountStatus);
 
-    const loggingServiceService = new LoggingServiceService();
-    loggingServiceService.logMessage(accountStatus);
+    //This is not a good practice of calling services.
+    // const loggingServiceService = new LoggingServiceService();
+    // loggingServiceService.logMessage(accountStatus);
+
+    this.loggingService.logMessage(accountStatus);
   }
 
 }
